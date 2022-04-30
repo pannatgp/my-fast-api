fc1e9f4b8a8216aeba7e2db14178298b9f9247aa
pannatgp <panna.tgp@gmail.com>
2022-05-01 06:37:31 +0700

    init project

---
+++ routers/users/users_router.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/users",
+    tags=["users"]
+)
+
+fake_user_db = [
+    {"username": "codewizz"},
+    {"username": "sonny"},
+]
+
+@router.get("/")
+def get_all_user():
+    return {}
