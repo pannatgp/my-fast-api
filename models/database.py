0ddb6afe96efbe8cb07096897d75481cbb1657cc
pannatgp <panna.tgp@gmail.com>
2022-05-01 07:02:17 +0700

    add: database config

---
+++ models/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./my-fast-api.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thred": False})
+
+session_local = sessionmaker(autocommit=False, autoFlush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    try:
+        db = session_local()
+        yield db
+    finally:
+        db.close()
+        
