2fdfbc82799c8351417ab19c1c3c45250610bde3
pannatgp <panna.tgp@gmail.com>
2022-05-01 08:19:21 +0700

    edited database.py

--- models/database.py
+++ models/database.py
@@ -1,21 +1,24 @@
+from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./my-fast-api.db"
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-session_local = sessionmaker(autocommit=False, autoFlush=False, bind=engine)
+seesion_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 
 def get_db():
     try:
-        db = session_local()
+        db = seesion_local()
         yield db
     finally:
         db.close()
